e772d59c177b5b8b685a3be973206c7759bf0239
yjhwang <hghyj719@gmail.com>
2025-11-30 04:53:27 +0900

    feat: dataset&datamodule

---
+++ arcface_lightning_v2/data/datamodule.py
@@ -0,0 +1,117 @@
+# worker_init_fn은 utils가 없을 수 있으므로 직접 구현
+import functools
+import random
+
+import lightning as L
+import numpy as np
+import torch
+from dataset import get_dataset
+from torch.utils.data import DataLoader
+
+
+def _worker_init_fn(worker_id, num_workers, rank, seed):
+    """Worker 초기화 함수 - 각 worker의 시드를 설정 (내부 함수)"""
+    worker_seed = num_workers * rank + worker_id + seed
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
+    torch.manual_seed(worker_seed)
+
+
+class ArcFaceDataModule(L.LightningDataModule):
+    """ArcFace 학습을 위한 Lightning DataModule"""
+
+    def __init__(
+        self,
+        root_dir: str,
+        batch_size: int = 128,
+        num_workers: int = 4,
+        seed: int = 2048,
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+
+        self.root_dir = root_dir
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.seed = seed
+
+        # 데이터셋은 setup에서 초기화
+        self.train_dataset = None
+        self.val_dataset = None
+        self.test_dataset = None
+
+    def setup(self, stage: str | None = None):
+        """
+        데이터셋 초기화
+        DDP 환경에서도 모든 프로세스에서 호출됨
+        """
+        if stage == "fit" or stage is None:
+            self.train_dataset = get_dataset(self.root_dir)
+            # ArcFace는 보통 validation 데이터셋이 없지만, 필요시 추가 가능
+            # self.val_dataset = get_dataset(self.root_dir, split="val")
+
+        if stage == "test" or stage is None:
+            self.test_dataset = get_dataset(self.root_dir)
+
+    def train_dataloader(self) -> DataLoader:
+        """학습용 DataLoader 반환"""
+        # worker_init_fn 설정
+        # lambda 대신 functools.partial을 사용하여 pickle 가능하게 만듦
+        if self.seed is not None:
+            rank = self.trainer.global_rank if self.trainer else 0
+            # functools.partial을 사용하여 pickle 가능한 함수 생성
+            init_fn = functools.partial(
+                _worker_init_fn,
+                num_workers=self.num_workers,
+                rank=rank,
+                seed=self.seed,
+            )
+        else:
+            init_fn = None
+
+        # Lightning이 자동으로 DistributedSampler를 추가하므로
+        # shuffle=True로 설정하면 됨
+        # pin_memory는 CUDA에서만 유효하므로, CUDA가 있을 때만 True
+        pin_memory = torch.cuda.is_available()
+
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+            pin_memory=pin_memory,
+            drop_last=True,
+            worker_init_fn=init_fn,
+        )
+
+    def val_dataloader(self) -> DataLoader | None:
+        """검증용 DataLoader 반환 (선택적)"""
+        if self.val_dataset is None:
+            return None
+
+        pin_memory = torch.cuda.is_available()
+
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=pin_memory,
+            drop_last=False,
+        )
+
+    def test_dataloader(self) -> DataLoader | None:
+        """테스트용 DataLoader 반환 (선택적)"""
+        if self.test_dataset is None:
+            return None
+
+        pin_memory = torch.cuda.is_available()
+
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=pin_memory,
+            drop_last=False,
+        )
---
+++ arcface_lightning_v2/data/dataset.py
@@ -0,0 +1,98 @@
+import numbers
+import os
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+
+# class MXFaceDataset(Dataset):
+#     """MXNet RecordIO 형식 데이터셋"""
+
+#     def __init__(self, root_dir: str):
+#         super().__init__()
+#         self.transform = transforms.Compose(
+#             [
+#                 transforms.ToPILImage(),
+#                 transforms.RandomHorizontalFlip(),
+#                 transforms.ToTensor(),
+#                 transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+#             ]
+#         )
+#         self.root_dir = root_dir
+#         path_imgrec = os.path.join(root_dir, "train.rec")
+#         path_imgidx = os.path.join(root_dir, "train.idx")
+
+#         self.imgrec = mx.recordio.MXIndexedRecordIO(path_imgidx, path_imgrec, "r")
+#         s = self.imgrec.read_idx(0)
+#         header, _ = mx.recordio.unpack(s)
+
+#         if header.flag > 0:
+#             self.header0 = (int(header.label[0]), int(header.label[1]))
+#             self.imgidx = np.array(range(1, int(header.label[0])))
+#         else:
+#             self.imgidx = np.array(list(self.imgrec.keys))
+
+#     def __getitem__(self, index):
+#         idx = self.imgidx[index]
+#         s = self.imgrec.read_idx(idx)
+#         header, img = mx.recordio.unpack(s)
+#         label = header.label
+
+#         if not isinstance(label, numbers.Number):
+#             label = label[0]
+#         label = torch.tensor(label, dtype=torch.long)
+
+#         sample = mx.image.imdecode(img).asnumpy()
+#         if self.transform is not None:
+#             sample = self.transform(sample)
+
+#         return sample, label
+
+#     def __len__(self):
+#         return len(self.imgidx)
+
+
+class SyntheticDataset(Dataset):
+    """테스트용 합성 데이터셋"""
+
+    def __init__(self):
+        super().__init__()
+        img = np.random.randint(0, 255, size=(112, 112, 3), dtype=np.int32)
+        img = np.transpose(img, (2, 0, 1))
+        img = torch.from_numpy(img).squeeze(0).float()
+        img = ((img / 255) - 0.5) / 0.5
+        self.img = img
+        self.label = 1
+
+    def __getitem__(self, index):
+        return self.img, self.label
+
+    def __len__(self):
+        return 1000000
+
+
+def get_dataset(root_dir: str) -> Dataset:
+    """
+    데이터셋 타입을 자동으로 감지하여 적절한 Dataset 반환
+
+    Args:
+        root_dir: 데이터셋 루트 디렉토리
+
+    Returns:
+        Dataset 인스턴스
+    """
+    # Synthetic 데이터셋
+    if root_dir == "synthetic":
+        return SyntheticDataset()
+
+    # ImageFolder 형식
+    transform = transforms.Compose(
+        [
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+        ]
+    )
+    return ImageFolder(root_dir, transform=transform)
