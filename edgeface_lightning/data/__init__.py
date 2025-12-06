028436feb61474e5eca097a275099286c0fabc54
yjhwang <hghyj719@gmail.com>
2025-12-06 14:23:44 +0900

    feat: edgeface datamodule

---
+++ edgeface_lightning/data/__init__.py
@@ -0,0 +1,4 @@
+# Lazy imports to avoid circular import issues
+# Import directly from submodules when needed:
+#   from edgeface_lightning.data.datamodule import EdgeFaceDataModule
+#   from edgeface_lightning.data.verification_dataset import VerificationPairsDataset
